fff30e53d13c2dae86f0163312b853e34ecd8f1b
EC2 Default User <ec2-user@ip-172-31-95-12.ec2.internal>
2019-05-04 02:47:14 +0000

    Script to create multiple EC2 instances.

--- ec2-scripts/ec2-create.sh
+++ ec2-scripts/ec2-create.sh
@@ -9,50 +9,30 @@ if [ $# -ne 2 ]; then
   exit 2
 fi
 
-DEBUG="echo "
-DEBUG=""
-
 CONFIG_FILE=$1
 NUM_INSTANCES=$2
 
-echo $1
-
 FILE_CONTENTS=`cat "$CONFIG_FILE"` 
-echo $FILE_CONTENTS
+echo "Json config file: " $FILE_CONTENTS
+
+# Filename for logging
+_nowDate=`date -u "+%Y%m%d-%H%M%S-%Z" | tr [A-Z] [a-z]`
+_logsDir="${HOME}/Documents/awscli-logs"   
+_logFile="awscli-ec2-create-$_nowDate.log"
+
 
 # ec2 parameters
-EC2_AMI=`cat "$CONFIG_FILE" | jq '.imageId'`
-EC2_INSTANCE=`cat "$CONFIG_FILE" | jq '.instanceType'`
-EC2_KEY=`cat "$CONFIG_FILE" | jq '.keyName'`
-EC2_SECURITY_GROUP_IDS=`cat "$CONFIG_FILE" | jq '.securityGroupIds'`
-EC2_BLOCK_DEVICE_MAPPINGS=`cat "$CONFIG_FILE" | jq '.blockDeviceMappings'`
-EC2_SUBNET_ID=`cat "$CONFIG_FILE" | jq '.subnetId'`
-EC2_USER_DATA=`cat "$CONFIG_FILE" | jq '.userDataFile'`
-EC2_TAG_SPECIFICATIONS=`cat "$CONFIG_FILE" | jq '.tagSpecifications'`
-echo "EC2 AMI" .  $EC2_AMI
-echo [$EC2_AMI]
-echo "INSTANCE" $EC2_INSTANCE
-echo EC2_KEY $EC2_KEY
-echo SG $EC2_SECURITY_GROUP_IDS
-echo subnet-id $EC2_SUBNET_ID
+EC2_AMI=`cat "$CONFIG_FILE" | jq -r '.imageId'`
+EC2_INSTANCE=`cat "$CONFIG_FILE" | jq -r '.instanceType'`
+EC2_KEY=`cat "$CONFIG_FILE" | jq -r  '.keyName'`
+EC2_SECURITY_GROUP_IDS=`cat "$CONFIG_FILE" | jq -r '.securityGroupIds'`
+EC2_BLOCK_DEVICE_MAPPINGS=`cat "$CONFIG_FILE" | jq -r '.blockDeviceMappings'`
+EC2_SUBNET_ID=`cat "$CONFIG_FILE" | jq -r '.subnetId'`
+EC2_USER_DATA=`cat "$CONFIG_FILE" | jq -r '.userDataFile'`
 
-`aws ec2 run-instances --image-id $EC2_AMI --instance-type $EC2_INSTANCE --key-name $EC2_KEY --subnet $EC2_SUBNET_ID --region us-east-1`
-#EC2_RUN=`aws ec2 run-instances --image-id "ami-0080e4c5bc078760e" --instance-type "t2.micro" --key-name "vasudha-key" --security-group-ids "sg-00994774d3b38e6f2" --subnet-id "subnet-f0ef8cde"`
-INSTANCE_NAME=$(echo ${EC2_RUN_RESULT} | sed 's/RESERVATION.*INSTANCE //' | sed 's/ .*//')
-echo "Instance Status is" $INSTANCE_NAME
+ec2_id=$(aws ec2 run-instances --image-id $EC2_AMI  --count $NUM_INSTANCES --instance-type $EC2_INSTANCE --key-name $EC2_KEY --subnet-id $EC2_SUBNET_ID --block-device-mappings $EC2_BLOCK_DEVICE_MAPPINGS --associate-public-ip-address >> ${_logsDir}/${_logFile})
 
-times=0
-echo
-while [ 5 -gt $times ] && ! ec2-describe-instances $INSTANCE_NAME | grep -q "running"
-do
-  times=$(( $times + 1 ))
-  echo Attempt $times at verifying $INSTANCE_NAME is running...
-done
+instance_id=$(cat ${_logsDir}/${_logFile} | grep InstanceId | cut -d":" -f2 | cut -d'"' -f2)
+echo "Instance Id of EC2 launched is" $instance_id 
 
-
-if [ 5 -eq $times ]; then
-  echo Instance $INSTANCE_NAME is not running. Exiting...
-  exit 0
-fi
-
-
+exit 0
